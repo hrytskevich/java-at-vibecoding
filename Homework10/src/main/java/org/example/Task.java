a49c3fa10098eda968f2d3e72d3ad5c203a700e5
Грицкевич Ольга Вячеславовна <OGritskevich@alfabank.ru>
2026-06-23 12:42:12 +0300

    ДЗ10 Описан класс Task

--- Homework10/src/main/java/org/example/Task.java
+++ Homework10/src/main/java/org/example/Task.java
@@ -1,4 +1,31 @@
 package org.example;
 
 public class Task {
+    private String taskName;
+    private boolean isDone;
+
+    public Task(String taskName) {
+        this.taskName = taskName;
+    }
+
+    public String getTaskName() {
+        return this.taskName;
+    }
+
+    public boolean isDone() {
+        return this.isDone;
+    }
+
+    public void makeDone() {
+        if (this.isDone) {
+            System.out.println("Задача уже была выполнена!");
+        } else {
+            this.isDone = true;
+            System.out.println("Успешно! Задача выполнена.");
+        }
+    }
+
+    public void printInfo() {
+        System.out.println((isDone ? "[✓] " : "[] ") + this.taskName);
+    }
 }
