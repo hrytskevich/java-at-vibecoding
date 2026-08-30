0b049992104058faee3a0bef0a5c99cc85bbf595
Грицкевич Ольга Вячеславовна <OGritskevich@alfabank.ru>
2026-08-30 13:45:19 +0300

    ДЗ19 Акт 1

--- Homework19/src/test/java/com.lesson.homework/HerokuappTests.java
+++ Homework19/src/test/java/com.lesson.homework/HerokuappTests.java
@@ -1,41 +1,45 @@
 package com.lesson.homework;
 
 import com.codeborne.selenide.Selenide;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.sleep;
+import static com.codeborne.selenide.Selenide.open;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class HerokuappTests {
 
     @Test
     public void testLoginLogout() {
-        Selenide.open("https://the-internet.herokuapp.com/");
-        Selenide.$x("//a[text()='Form Authentication']").click();
-        Selenide.sleep(1000);
-        Assertions.assertEquals("Login Page",Selenide.$x("//div[@class='example']/h2").getText());
-        Selenide.$x("//input[@name='username']").setValue("tomsmith");
-        Selenide.$x("//input[@name='password']").setValue("SuperSecretPassword!");
-        Selenide.$x("//i[contains(text(),'Login')]/..").click();
-        Selenide.sleep(1000);
-        Assertions.assertTrue(Selenide.$x("//div[@id='flash-messages']/div").text().contains("You logged into a secure area!"));
-        Selenide.$x("//i[contains(text(),'Logout')]/..").shouldBe(visible);
-        Selenide.$x("//i[contains(text(),'Logout')]/..").click();
-        Selenide.sleep(1000);
-        Assertions.assertEquals("Login Page",Selenide.$x("//div[@class='example']/h2").getText());
+        open("https://the-internet.herokuapp.com/");
+        $x("//a[text()='Form Authentication']").click();
+        sleep(1000);
+        assertEquals("Login Page",Selenide.$x("//div[@class='example']/h2").getText());
+        $x("//input[@name='username']").setValue("tomsmith");
+        $x("//input[@name='password']").setValue("SuperSecretPassword!");
+        $x("//i[contains(text(),'Login')]/..").click();
+        sleep(1000);
+        assertTrue(Selenide.$x("//div[@id='flash-messages']/div").text().contains("You logged into a secure area!"));
+        $x("//i[contains(text(),'Logout')]/..").shouldBe(visible);
+        $x("//i[contains(text(),'Logout')]/..").click();
+        sleep(1000);
+        assertEquals("Login Page",Selenide.$x("//div[@class='example']/h2").getText());
     }
 
     @Test
     public void testLoginNegative() {
-        Selenide.open("https://the-internet.herokuapp.com/");
-        Selenide.$x("//a[text()='Form Authentication']").click();
-        Selenide.sleep(1000);
-        Selenide.$x("//a[contains(text(),'Elemental Selenium')]").shouldBe(visible);
-        Selenide.$x("//input[@name='username']").setValue("admin");
-        Selenide.$x("//input[@name='password']").setValue("1234");
-        Selenide.$x("//i[contains(text(),'Login')]/..").click();
-        Selenide.sleep(1000);
-        Assertions.assertTrue(Selenide.$x("//div[@id='flash-messages']/div").text().contains("Your username is invalid!"));
+        open("https://the-internet.herokuapp.com/");
+        $x("//a[text()='Form Authentication']").click();
+        sleep(1000);
+        $x("//a[contains(text(),'Elemental Selenium')]").shouldBe(visible);
+        $x("//input[@name='username']").setValue("admin");
+        $x("//input[@name='password']").setValue("1234");
+        $x("//i[contains(text(),'Login')]/..").click();
+        sleep(1000);
+        assertTrue(Selenide.$x("//div[@id='flash-messages']/div").text().contains("Your username is invalid!"));
     }
 
 
